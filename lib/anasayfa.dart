6faa896cef800e87934836f9c810163a778afbaa
serdarpolat <serdar.plt21@gmail.com>
2018-10-06 00:27:04 +0300

    Home page dsign started

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -31,46 +31,102 @@ class HomePage extends StatelessWidget {
         elevation: 0.0,
       ),
       body: Container(
-        color: Colors.white,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(width: 1.0, color: Colors.black12),
+            bottom: BorderSide(width: 1.0, color: Colors.black12),
+          ),
+        ),
         child: Column(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(width: 1.0, color: Colors.black12),
-                  bottom: BorderSide(width: 1.0, color: Colors.black12),
-                ),
-              ),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 18.0),
-                      child: Text(
-                        'Filitrele',
-                        textDirection: TextDirection.ltr,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontFamily: 'Josefin',
-                          fontSize: 16.0,
-                        ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text(
+                      'Filitrele',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontFamily: 'Josefin',
+                        fontSize: 16.0,
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 18.0),
-                      child: Text(
-                        'Sırala',
-                        textDirection: TextDirection.ltr,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontFamily: 'Josefin',
-                          fontSize: 16.0,
-                        ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 18.0),
+                    child: Text(
+                      'Sırala',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontFamily: 'Josefin',
+                        fontSize: 16.0,
                       ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 1.0, color: Colors.black12),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    elevation: 5.0,
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset(
+                          './images/woman_1.png',
+                          width: (MediaQuery.of(context).size.width/2)-40.0,
+                          height: 240.0,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text('320,00 TL'),
+                              Text('460,00 TL', style: TextStyle(color: Colors.grey, decoration: TextDecoration.lineThrough),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    elevation: 5.0,
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset(
+                          './images/man_1.png',
+                          width: (MediaQuery.of(context).size.width/2)-40.0,
+                          height: 240.0,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text('320,00 TL'),
+                              Text('460,00 TL', style: TextStyle(color: Colors.grey, decoration: TextDecoration.lineThrough),)
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
