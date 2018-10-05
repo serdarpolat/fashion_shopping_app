ad7c31039649574b7b4a804112424c5eb9311024
serdarpolat <serdar.plt21@gmail.com>
2018-10-05 22:42:36 +0300

    Some changes

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -6,39 +6,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0),
-          child: Container(
-            decoration: new BoxDecoration(
-              border: new Border(
-                bottom: new BorderSide(width: 1.0, color: Colors.black12),
-              ),
-            ),
-          ),
-        ),
         bottomOpacity: 1.0,
         backgroundColor: Colors.white,
         centerTitle: true,
         leading: IconButton(
-            icon: Icon(Icons.menu), color: Colors.black, onPressed: () {}),
+            icon: Icon(Icons.menu), color: Colors.black54, onPressed: () {}),
         title: Text(
           'FASHIONISM',
           style: TextStyle(
               color: Colors.black, fontFamily: 'Josefin', fontSize: 16.0),
         ),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), color: Colors.black, onPressed: () {},),
-          IconButton(icon: Icon(Icons.shopping_basket), color: Colors.black, onPressed: () {},),
+          IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.black54,
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_basket),
+            color: Colors.black54,
+            onPressed: () {},
+          ),
         ],
         elevation: 0.0,
       ),
       body: Container(
         color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text('Merhaba'),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(width: 1.0, color: Colors.black12),
+              bottom: BorderSide(width: 1.0, color: Colors.black12),
+            ),
+          ),
+          child: Text('Selam'),
         ),
       ),
     );
