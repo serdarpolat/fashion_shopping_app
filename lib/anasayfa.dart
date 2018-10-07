52a892892a3e48e0fcffd8e3158b4f4afdb51c49
serdarpolat <serdar.plt21@gmail.com>
2018-10-07 13:03:49 +0300

    Home page design continues

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -12,9 +12,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        bottomOpacity: 1.0,
+        elevation: 0.0,
         backgroundColor: Colors.white,
-        centerTitle: false,
+        centerTitle: true,
         leading: IconButton(
             icon: Icon(Icons.menu), color: Colors.black54, onPressed: () {}),
         title: Text(
@@ -34,7 +34,16 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {},
           ),
         ],
-        elevation: 0.0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(0.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(width: 1.0, color: Colors.black12),
+              ),
+            ),
+          ),
+        ),
       ),
       body: new MainContent(),
     );
@@ -58,6 +67,387 @@ class MainContent extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontSize: 30.0,
                   ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Women Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'WOMAN',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.pink.shade200, BlendMode.multiply),
+                              image: AssetImage('./images/cat_woman.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Men Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5, right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'MAN',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.blue.shade400, BlendMode.multiply),
+                              image: AssetImage('./images/cat_man.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Kids Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'KIDS',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.green.shade400, BlendMode.multiply),
+                              image: AssetImage('./images/cat_kids.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Discounts',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Women Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'EOTS\nDiscount',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.pink.shade200, BlendMode.multiply),
+                              image: AssetImage('./images/cat_woman_1.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Men Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5, right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '%70 Discount\nDon\'t be late!',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.brown.shade400, BlendMode.multiply),
+                              image: AssetImage('./images/cat_man_1.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go EOTM Discount Kids Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'EOTM\nDiscount',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.amberAccent.shade400, BlendMode.multiply),
+                              image: AssetImage('./images/cat_group.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Accessories',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Jewelry Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Jewelry',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFF03DAC6), BlendMode.multiply),
+                              image: AssetImage('./images/jewelry.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go To Watch Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5, right: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Watch',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFFF4511E), BlendMode.multiply),
+                              image: AssetImage('./images/watch.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          print('Go Others Category');
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(left: 2.5),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Others',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(1.0, 2.0),
+                                blurRadius: 2.0,
+                              ),
+                            ],
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFF00B0FF), BlendMode.multiply),
+                              image: AssetImage('./images/others.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
